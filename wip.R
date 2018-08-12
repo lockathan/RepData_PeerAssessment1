50220dd45b4834a380f44a33e8e1b2009cca183f
Jonathan Locke <lockathan@gmail.com>
2018-08-12 20:08:12 +0200

    Deleted unnecessary files

--- wip.R
+++
@@ -1,9 +0,0 @@
-activity = read.csv("activity.csv")
-
-totalStepsPerDay <- aggregate(steps ~ date, activity, FUN = sum)
-hist(totalStepsPerDay$steps)
-
-
-averageSteps <- aggregate(steps ~ interval, activity, FUN = mean)
-
-plot(averageSteps$steps ~ averageSteps$interval, type = "l")
